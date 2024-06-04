40fca76f8e9d6c34c8cf7517f16405ea58f50662
Achyut <achyutparajuli49@gmail.com>
2024-06-04 21:26:33 +0545

    menu and link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('review-qr-link', function () {
+    $url = "https://reviewthis.biz/Deurali-Marwadi-Hotel-and-Restaurant";
+    return Redirect::to($url);
+});
+
+Route::get('menu', function () {
+    $pathToFile = public_path('Deurali Marwadi Menu.pdf');
+    return response()->file($pathToFile);
 });
